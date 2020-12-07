b24987d3b834be9db0928a20256d08a18eb66810
Anne Wieggers <Anne.Wieggers@au.ey.com>
2020-12-07 15:44:12 +1100

    updated code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,11 +3,36 @@ import csv
 import os
 
 # set path for file
-csvpath = os.path.join('..', 'PyBank', 'Resources', 'election_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 # open and read csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+# define the function and have it accept the budget_data as sole parameter
+
+    # set variables and assign values where possible 
+    total_votes = 0 
+    candidates = []
+    votes = 0
+    votes_khan = 0
+    perc_khan = 0
+    votes_correy = 0
+    perc_correy = 0
+    votes_li = 0
+    perc_li = 0
+    votes_otooley = 0
+    perc_otooley = 0
+
+for row in csvreader:
+
+    total_votes = total_votes + 1
+
+    candidates.append(row[2])
+    
+    #for candidates in csvreader
+    votes.append(candidates)
+    #votes_khan = votes_khan + int(votes_data[0]
 
 print("Election Results")    
 print("-------------------------")
